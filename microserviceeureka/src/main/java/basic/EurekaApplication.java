57736058d0e725ef0912ccc5419ade0e87cafb69
Hongscar <445673440@qq.com>
2019-11-28 10:25:35 +0800

    connect Eureka

---
+++ microserviceeureka/src/main/java/basic/EurekaApplication.java
@@ -0,0 +1,18 @@
+package basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: Seth
+ * @Description:
+ * @Date: Created in 10:17 2019/11/28
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
